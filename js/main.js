44fca5a59c530fbb1c58d36e7669e916bbd95a2f
Joey Panganiban <joeympanganiban30@gmail.com>
2021-09-28 11:43:14 +0800

    new

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 const links = [
-  { label: "Week1", url: "week1/index.html" },
-  { label: "Week2", url: "week2/index.html" },
-  { label: "Week3", url: "week3/index.html" },
-  { label: "Week4", url: "week4/index.html" },
-  { label: "Week5", url: "week5/index.html" },
-  { label: "Week6", url: "week6/index.html" },
-  { label: "Week7", url: "week7/index.html" },
-  { label: "Week8", url: "week8/index.html" },
-  { label: "Week9", url: "week9/index.html" },
-  { label: "Week10", url: "week10/index.html" },
-  { label: "Week11", url: "week10/index.html" },
-  { label: "Week12", url: "week10/index.html" },
-  { label: "Week13", url: "week10/index.html" },
+  { label: "Week1", url: "week1/index.html", notes: "Local Storage, Portfolio, UX Mobile Design" },
+  { label: "Week2", url: "week2/index.html", notes: " " },
+  { label: "Week3", url: "week3/index.html", notes: " " },
+  { label: "Week4", url: "week4/index.html", notes: " " },
+  { label: "Week5", url: "week5/index.html", notes: " " },
+  { label: "Week6", url: "week6/index.html", notes: " " },
+  { label: "Week7", url: "week7/index.html", notes: " " },
+  { label: "Week8", url: "week8/index.html", notes: " " },
+  { label: "Week9", url: "week9/index.html", notes: " " },
+  { label: "Week10", url: "week10/index.html", notes: " " },
+  { label: "Week11", url: "week10/index.html", notes: " " },
+  { label: "Week12", url: "week10/index.html", notes: " " },
+  { label: "Week13", url: "week10/index.html", notes: " " },
   ]
 
 const list = document.getElementById('weekList');
@@ -20,12 +20,15 @@ for (let i=0; i<links.length; i++){
   // create an element <li> <a></a> </li> inside <ol></ol>
   let li = document.createElement('li');
   let a  = document.createElement('a');
+  let em = document.createElement('em')
 
-  a.textContent = links[i].label;  // set a Label of Week[i]
   a.setAttribute('href', links[i].url); //set a link/url for Week[i]
+  a.textContent = links[i].label + " : " + links[i].notes;  // set a Label of Week[i]
 
   li.appendChild(a); // put the a inside the li
   list.appendChild(li); // put the li inside the #list
+
+
 };
 
 
