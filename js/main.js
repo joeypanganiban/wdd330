4f94d28e780f7567a28fd0ef1d49fbb621beb07a
Joey Panganiban <joeympanganiban30@gmail.com>
2021-09-28 08:02:13 +0800

    ccc

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const links = [
     {
-      label: "Week1 notes",
+      label: "Week1",
       url: "week1/index.html"
+    },
+    {
+      label: "Week2",
+      url: "week2/index.html"
     }
   ]
+
+  const message = 'Hello World!'
+  alert(message);
+
+  let label;
+  let url;
+
+
+  
+  
