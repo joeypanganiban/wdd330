67b9c681f9ff4d813f42b4ef3a492d0c364a2b43
Joey Panganiban <joeympanganiban30@gmail.com>
2021-09-28 08:14:28 +0800

    hep

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 const links = [
-    {
-      label: "Week1",
-      url: "week1/index.html"
-    },
-    {
-      label: "Week2",
-      url: "week2/index.html"
-    }
+  { label: "Week1", url: "week1/index.html" },
+  { label: "Week2", url: "week2/index.html" },
+  { label: "Week3", url: "week3/index.html" },
+  { label: "Week4", url: "week4/index.html" },
+  { label: "Week5", url: "week5/index.html" },
+  { label: "Week6", url: "week6/index.html" },
+  { label: "Week7", url: "week7/index.html" },
+  { label: "Week8", url: "week8/index.html" },
+  { label: "Week9", url: "week9/index.html" },
+  { label: "Week10", url: "week10/index.html" },
+  { label: "Week11", url: "week10/index.html" },
+  { label: "Week12", url: "week10/index.html" },
+  { label: "Week13", url: "week10/index.html" },
   ]
 
-  const message = 'Hello World!'
-  alert(message);
+let weeksList;
+weeksList = links;
 
-  let label;
-  let url;
+document.createElement('div');
+
+for(let i=0; i<=weeksList.length; i++) {
+};
 
 
-  
+
   
