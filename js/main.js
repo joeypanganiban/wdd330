c81335af491a467887700df74bc231844fa2659a
Joey Panganiban <joeympanganiban30@gmail.com>
2021-09-28 19:15:18 +0800

    sett

--- js/main.js
+++ js/main.js
@@ -20,17 +20,12 @@ for (let i=0; i<links.length; i++){
   // create an element <li> <a></a> </li> inside <ol></ol>
   let li = document.createElement('li');
   let a  = document.createElement('a');
-  let em = document.createElement('em')
 
   a.setAttribute('href', links[i].url); //set a link/url for Week[i]
   a.textContent = links[i].label + " : " + links[i].notes;  // set a Label of Week[i]
 
   li.appendChild(a); // put the a inside the li
   list.appendChild(li); // put the li inside the #list
-
-
 };
-
-
 
   
