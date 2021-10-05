da82e603a050faf4c6f9b369dfcc82d03909f542
Joey Panganiban <joeympanganiban30@gmail.com>
2021-10-06 01:39:59 +0800

    end ch3

--- week2/quiz/main.js
+++ week2/quiz/main.js
@@ -1,30 +1,37 @@
-alert('Welcome to Quiz Ninja!');
-
-const question = "What is your name?"
-const answer = prompt(question);
-alert(`Hello, ${answer}`);
-
-const age = 23;
-if (age < 12) {
-console.log('Sorry, you are not old enough to play this game');
-} else {
-console.log('good')
-}
-
 const quiz = [
     ["What is Superman's real name?","Clark Kent"],
     ["What is Wonder Woman's real name?","Diana Prince"],
     ["What is Batman's real name?","Bruce Wayne"]
 ];
-let score = 0 // initialize score
-for(const [question,answer] of quiz){
-    const response = prompt(question);
-    if(response === answer){
+
+function start(quiz){
+    let score = 0;
+
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
+    // end of main game loop
+
+    gameOver();
+
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(response,answer){
+        if(response === answer){
         alert('Correct!');
         score++;
-    } else {
+        } else {
         alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
     }
 }
-// At the end of the game, report the player's score
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+start(quiz);
